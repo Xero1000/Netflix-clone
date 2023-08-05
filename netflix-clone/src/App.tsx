ace2b023520a38e1f0f56c1249a5cf4df66669f4
Xero1000 <alex.heil@comcast.net>
2023-08-05 11:07:11 -0400

    Build a grid layout

--- netflix-clone/src/App.tsx
+++ netflix-clone/src/App.tsx
@@ -1,10 +1,21 @@
+import { Grid, GridItem } from "@chakra-ui/react"
 
 
 function App() {
 
   return (
     <>
-      
+      <Grid
+        templateAreas={`"nav"
+                        "billboard"
+                        "sliders"
+                        "footer"`}
+      >
+        <GridItem area={"nav"}>NavBar</GridItem>
+        <GridItem area={"billboard"}>Billboard</GridItem>
+        <GridItem area={"sliders"}>Sliders</GridItem>
+        <GridItem area={"footer"}>Footer</GridItem>
+      </Grid>
     </>
   )
 }
