577bb7bb53516d14406df3935e658be04b709c38
Xero1000 <alex.heil@comcast.net>
2023-08-14 11:42:31 -0400

    Replace react multi carousel with nuka-carousel

--- netflix-clone/src/components/Slider.tsx
+++ netflix-clone/src/components/Slider.tsx
@@ -1,9 +1,10 @@
 import { Box, Heading, Image } from "@chakra-ui/react";
-import Carousel from "react-multi-carousel";
+import Carousel from 'nuka-carousel'; 
 import getFullPosterPath from "../utilities/getFullPosterPath";
 import { Movie } from "../entities/Movie";
 import { Tv } from "../entities/Tv";
 import LazyLoad from 'react-lazy-load';
+import { useEffect, useState } from "react";
 
 interface Props {
   label: string;
@@ -11,33 +12,33 @@ interface Props {
 }
 
 const Slider = ({ label, data }: Props) => {
-  const responsive = {
-    "2xl": {
-      breakpoint: { max: Infinity, min: 1399 },
-      items: 6,
-      slidesToSlide: 6,
-    },
-    xl: {
-      breakpoint: { max: 1398, min: 1099 },
-      items: 5,
-      slidesToSlide: 5,
-    },
-    lg: {
-      breakpoint: { max: 1098, min: 799 },
-      items: 4,
-      slidesToSlide: 4,
-    },
-    md: {
-      breakpoint: { max: 798, min: 499 },
-      items: 3,
-      slidesToSlide: 3,
-    },
-    sm: {
-      breakpoint: { max: 498, min: 0 },
-      items: 2,
-      slidesToSlide: 2,
-    },
-  };
+
+  // The number of slides to show depending on the size of
+  // the browser window 
+  const getSlidesToShow = (width: number) => {
+    if (width > 1399) return 6;
+    if (width > 1099) return 5;
+    if (width > 799) return 4;
+    if (width > 499) return 3;
+
+    return 2;
+  }
+
+  const [slidesToShow, setSlidesToShow] = useState(getSlidesToShow(window.innerWidth))
+
+  // changes the number of slides displayed based on browser size
+  useEffect(() => {
+    const handleResize = () => {
+      setSlidesToShow(getSlidesToShow(window.innerWidth));
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up by removing the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <Box py={3}>
@@ -46,18 +47,18 @@ const Slider = ({ label, data }: Props) => {
       </Heading>
       <LazyLoad offset={100}>
         <Carousel
-          responsive={responsive}
-          infinite={true}
-          centerMode={true}
-          draggable={false}
-          >
+          slidesToShow={slidesToShow} // Set the number of slides to show
+          wrapAround={true} // Enable infinite sliding
+          dragging={false} // Disable dragging
+          renderBottomCenterControls={null}
+        >
           {data?.map((movie) => (
             <Image
               key={movie.id}
               src={getFullPosterPath(movie.backdrop_path, "300")}
               p={1}
               borderRadius="8px"
-              />
+            />
           ))}
         </Carousel>
       </LazyLoad>
