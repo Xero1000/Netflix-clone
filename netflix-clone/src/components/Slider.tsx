3106db4980767c5c1efd918ae4fadf1c56d899fa
Xero1000 <alex.heil@comcast.net>
2023-08-14 11:56:33 -0400

    Added lazy load to nuka carousel

--- netflix-clone/src/components/Slider.tsx
+++ netflix-clone/src/components/Slider.tsx
@@ -1,9 +1,9 @@
 import { Box, Heading, Image } from "@chakra-ui/react";
-import Carousel from 'nuka-carousel'; 
+import Carousel from "nuka-carousel";
 import getFullPosterPath from "../utilities/getFullPosterPath";
 import { Movie } from "../entities/Movie";
 import { Tv } from "../entities/Tv";
-import LazyLoad from 'react-lazy-load';
+import LazyLoad from "react-lazy-load";
 import { useEffect, useState } from "react";
 
 interface Props {
@@ -12,9 +12,8 @@ interface Props {
 }
 
 const Slider = ({ label, data }: Props) => {
-
   // The number of slides to show depending on the size of
-  // the browser window 
+  // the browser window
   const getSlidesToShow = (width: number) => {
     if (width > 1399) return 6;
     if (width > 1099) return 5;
@@ -22,9 +21,11 @@ const Slider = ({ label, data }: Props) => {
     if (width > 499) return 3;
 
     return 2;
-  }
+  };
 
-  const [slidesToShow, setSlidesToShow] = useState(getSlidesToShow(window.innerWidth))
+  const [slidesToShow, setSlidesToShow] = useState(
+    getSlidesToShow(window.innerWidth)
+  );
 
   // changes the number of slides displayed based on browser size
   useEffect(() => {
@@ -32,11 +33,11 @@ const Slider = ({ label, data }: Props) => {
       setSlidesToShow(getSlidesToShow(window.innerWidth));
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     // Clean up by removing the event listener when the component unmounts
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -48,17 +49,19 @@ const Slider = ({ label, data }: Props) => {
       <LazyLoad offset={100}>
         <Carousel
           slidesToShow={slidesToShow} // Set the number of slides to show
+          slidesToScroll={slidesToShow} // Number of slides to scroll with each button click
           wrapAround={true} // Enable infinite sliding
           dragging={false} // Disable dragging
           renderBottomCenterControls={null}
         >
           {data?.map((movie) => (
-            <Image
-              key={movie.id}
-              src={getFullPosterPath(movie.backdrop_path, "300")}
-              p={1}
-              borderRadius="8px"
-            />
+            <LazyLoad key={movie.id}>
+              <Image
+                src={getFullPosterPath(movie.backdrop_path, "300")}
+                p={1}
+                borderRadius="8px"
+              />
+            </LazyLoad>
           ))}
         </Carousel>
       </LazyLoad>
