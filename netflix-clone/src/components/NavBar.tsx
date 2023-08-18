a29fae5d44cb65d56ff9098dfab0daa544908e77
Xero1000 <alex.heil@comcast.net>
2023-08-18 09:57:14 -0400

    Implement navigation

--- netflix-clone/src/components/NavBar.tsx
+++ netflix-clone/src/components/NavBar.tsx
@@ -1,23 +1,19 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/netflix.png";
 import { GoSearch } from "react-icons/go";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  const items = [
-    "Home",
-    "TV Shows",
-    "Movies",
-  ];
 
   return (
     <HStack justifyContent="space-between" paddingX={10}>
       <HStack gap={5}>
         <Image src={logo} boxSize="5em" />
-        {items.map((item, index) => (
-          <Text key={index} fontSize="1vw">
-            {item}
-          </Text>
-        ))}
+        <HStack fontSize="1vw" gap={5}>
+          <Link to="/">Home</Link>
+          <Link to="/tvshows">TV Shows</Link>
+          <Link to="/movies">Movies</Link>
+        </HStack>
       </HStack>
       <GoSearch size="25px" />
     </HStack>
