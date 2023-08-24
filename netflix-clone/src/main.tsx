ad64405ca7047f37c2fc88db4a34712eaabacc7b
Xero1000 <alex.heil@comcast.net>
2023-08-24 09:35:12 -0400

    Change default color theme

--- netflix-clone/src/main.tsx
+++ netflix-clone/src/main.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
@@ -6,12 +6,14 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./services/routes";
+import theme from "./theme";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
         <ReactQueryDevtools />
--- netflix-clone/src/theme.ts
+++ netflix-clone/src/theme.ts
@@ -1,4 +1,8 @@
-import { extendTheme } from "@chakra-ui/react";
+import { ThemeConfig, extendTheme } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+    initialColorMode: "dark"
+}
 
 const breakpoints = {
     sm: '499px',
@@ -8,6 +12,19 @@ const breakpoints = {
     '2xl': '1699px',
 }
 
-const theme = extendTheme({ breakpoints })
+const theme = extendTheme({ breakpoints, config, colors: {
+    gray: {
+        50: "#f9f9f9",
+        100: "#ededed",
+        200: "#d3d3d3",
+        300: "#b3b3b3",
+        400: "#a0a0a0",
+        500: "#898989",
+        600: "#6c6c6c",
+        700: "#202020",
+        800: "#121212",
+        900: "#100"
+    }
+} })
 
 export default theme
