ad7032a119f3fa296ab6a5af1249a6dcf1a953c6
Xero1000 <alex.heil@comcast.net>
2023-08-31 10:50:42 -0400

    Move TrailerPage's load spinner to center of page

--- netflix-clone/src/pages/TrailerPage.tsx
+++ netflix-clone/src/pages/TrailerPage.tsx
@@ -1,14 +1,19 @@
 import { useParams } from "react-router-dom";
 import useVideos from "../hooks/useVideos";
 import getFirstTrailer from "../utilities/getFirstTrailer";
-import { Spinner, Text } from "@chakra-ui/react";
+import { Box, Spinner, Text } from "@chakra-ui/react";
 import styles from "../css-modules/trailer.module.css";
 
 const TrailerPage = () => {
   const params = useParams();
   const { data, isLoading, error } = useVideos(params.contentType!, parseInt(params.id!));
 
-  if (isLoading) return <Spinner />;
+  if (isLoading) return (
+    <Box w="100vw" h="100vh" display="flex" justifyContent="center" alignItems="center">
+      <Spinner color="#E50914"/>
+    </Box>
+  );
+
   if (error) return <Text>{error.message}</Text>;
 
   const trailer = getFirstTrailer(data?.results);
